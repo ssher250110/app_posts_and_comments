c5ada5d1fa00aeebb8a1ff8b0165dc1c38493475
ssher <ssher250110@yandex.ru>
2024-08-22 23:05:36 +0300

    added docstring

--- users/serializers.py
+++ users/serializers.py
@@ -4,10 +4,13 @@ from users.models import User
 
 
 class UserSerializers(ModelSerializer):
+    """Сериализатор пользователя"""
+
     class Meta:
         model = User
         fields = ["username", "password", "email", "phone", "birth_date"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
+        """Сохранение объекта"""
         return User.objects.create_user(**validated_data)
