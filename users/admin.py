ed226357b4cbfbcf71c2e07c49cc5f3ca378862b
ssher <ssher250110@yandex.ru>
2024-08-22 23:09:11 +0300

    added admin panel user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "username",
+        "email",
+        "phone",
+        "birth_date",
+        "created_at",
+        "updated_at",
+        "role",
+        "is_active",
+        "last_login",
+    ]
