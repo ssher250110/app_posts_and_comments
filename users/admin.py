8f1d7c0ac3573dcae37841eef50054d851ea1630
ssher <ssher250110@yandex.ru>
2024-08-26 21:52:00 +0300

    added docstring

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,8 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    """Отображение информации о пользователях в админ панели"""
+
     list_display = [
         "id",
         "username",
