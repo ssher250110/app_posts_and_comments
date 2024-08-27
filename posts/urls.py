4384374f45659e0cd8484f8e44be726ff6f999f0
ssher <ssher250110@yandex.ru>
2024-08-27 20:39:20 +0300

    added path CommentViewSet

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
 from posts.apps import PostsConfig
-from posts.views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
+from posts.views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView, CommentViewSet
+
+comment_router = SimpleRouter()
+comment_router.register("comments", CommentViewSet, basename="comments")
 
 app_name = PostsConfig.name
 
 urlpatterns = [
     path("posts/", PostListCreateAPIView.as_view(), name="posts-list-create"),
-    path("posts/<pk:int>/", PostRetrieveUpdateDestroyAPIView.as_view(), name="posts-retrieve-update-destroy"),
+    path("posts/<int:pk>/", PostRetrieveUpdateDestroyAPIView.as_view(), name="posts-retrieve-update-destroy"),
+    path("", include(comment_router.urls)),
 ]
