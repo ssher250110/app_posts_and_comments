27c78143dea02bfee59c66825fc2514a91b0a5ab
ssher <ssher250110@yandex.ru>
2024-08-27 18:17:28 +0300

    ruff format

--- posts/models.py
+++ posts/models.py
@@ -5,14 +5,20 @@ NULLABLE = {"null": True, "blank": True}
 
 
 class Post(models.Model):
-    """ Модель поста """
+    """Модель поста"""
 
     title = models.CharField(max_length=50, verbose_name="Название поста", help_text="Укажите название поста")
     text = models.TextField(**NULLABLE, verbose_name="Текст поста", help_text="Укажите текст поста")
-    image = models.ImageField(upload_to="post/image", **NULLABLE, verbose_name="Изображение",
-                              help_text="Загрузите изображение")
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
-                               verbose_name="Автор объявления", help_text="Укажите автора объявления")
+    image = models.ImageField(
+        upload_to="post/image", **NULLABLE, verbose_name="Изображение", help_text="Загрузите изображение"
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        verbose_name="Автор объявления",
+        help_text="Укажите автора объявления",
+    )
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата редактирования")
@@ -26,10 +32,15 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    """ Модель комментария """
+    """Модель комментария"""
 
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
-                               verbose_name="Автор комментария", help_text="Укажите автора комментария")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        verbose_name="Автор комментария",
+        help_text="Укажите автора комментария",
+    )
     text = models.TextField(verbose_name="Текст комментария", help_text="Добавьте текст комментария")
     post = models.ForeignKey("Post", on_delete=models.CASCADE, verbose_name="Пост", help_text="Укажите Пост")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
