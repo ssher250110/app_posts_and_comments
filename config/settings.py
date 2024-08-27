27fdacaa7a3bf7c7264b83c9e5d96a2d9a998aa9
ssher <ssher250110@yandex.ru>
2024-08-27 22:46:04 +0300

    added drf-spectacular

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "posts.apps.PostsConfig",
     "rest_framework",
     "corsheaders",
+    "drf_spectacular"
 ]
 
 MIDDLEWARE = [
@@ -149,6 +150,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
